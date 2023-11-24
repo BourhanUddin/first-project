6411153077227395559891b767fbc4a0652fb689
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-11-25 01:46:51 +0600

    fix some error in server,js

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,7 +4,7 @@ import path from "path";
 // to get current directory
 // console.log(process.cwd())
 dotenv.config({ path: path.join(process.cwd(), ".env") });
-
+// console.log(path);
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
--- src/app/server.ts
+++ src/app/server.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 import app from "./app";
-import config from "./config";
+import config from "./config/index";
 
 async function main() {
   try {
+    console.log("Connecting to MongoDB...");
+
     await mongoose.connect(config.database_url as string);
+    console.log("MongoDB URI:", config.database_url as string);
+    console.log("Connected to MongoDB successfully");
+
     app.listen(config.port, () => {
       console.log(`Example app listening on PORT ${config.port}`);
     });
